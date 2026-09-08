896a14895c2d30ce36b82791433bc1c15589f48b
Mahreen Asama <79749919+mahreenasama@users.noreply.github.com>
2026-09-08 20:18:05 +0500

    Update post_generator.py

--- post_generator.py
+++ post_generator.py
@@ -1,5 +1,6 @@
 # post_generator.py
 
+import os
 import smtplib
 from email.mime.text import MIMEText
 
@@ -15,8 +16,12 @@ msg["Subject"] = "LinkedIn Post Draft"
 msg["From"] = "media.agent26@gmail.com"
 msg["To"] = "media.agent26@gmail.com"
 
+email_user = os.environ["EMAIL_USER"]
+email_password = os.environ["EMAIL_PASSWORD"]
+
 server = smtplib.SMTP("smtp.gmail.com", 587)
 server.starttls()
 server.login(email_user, email_password)
 server.send_message(msg)
 server.quit()
+
