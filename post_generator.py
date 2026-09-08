615de98754459e6b87fb87c0b602952dba117855
Mahreen Asama <79749919+mahreenasama@users.noreply.github.com>
2026-09-08 18:09:34 +0500

    Update post_generator.py

--- post_generator.py
+++ post_generator.py
@@ -1,1 +1,22 @@
+# post_generator.py
 
+import feedparser
+
+rss = feedparser.parse(
+    "https://spring.io/blog.atom"
+)
+
+latest = rss.entries[0]
+title = latest.title
+link = latest.link
+
+post = f"""
+🚀 Interesting Spring update today:
+{title}
+I found this topic particularly interesting because it shows how the Spring ecosystem continues evolving.
+Read more:
+{link}
+#Java #SpringBoot #Microservices
+"""
+print(post)
+``
